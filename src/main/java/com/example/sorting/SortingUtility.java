4f6d198a4231d2c93edaf128e7a9bb4d50352290
spyro1001 <raymond.hedgecock@gmail.com>
2023-11-17 15:12:25 -0700

    Done

--- src/main/java/com/example/sorting/SortingUtility.java
+++ src/main/java/com/example/sorting/SortingUtility.java
@@ -5,19 +5,70 @@ public class SortingUtility {
 
     public static <T extends Comparable<T>> void gnomeSort(T[] data) {
 
-        // TODO implement Gnome Sort here
+        int position = 0;
+
+        while (position < data.length) {
+            if (position == 0 || data[position].compareTo(data[position -1]) >= 0){
+                position++;
+            } else {
+                T temp = data[position];
+                data[position] = data[position - 1];
+                data[position - 1] = temp;
+                position--;
+            }
+        }
     }
 
 
     public static <T extends Comparable<T>> void cocktailShakerSort(T[] data) {
+        boolean swapped;
+        do {
+            swapped = false;
+            for (int i = 0; i < data.length - 1; i++) {
+                if (data[i].compareTo(data[i + 1]) > 0) {
 
-        // TODO implement Cocktail Shaker Sort here
+                    T temp = data[i];
+                    data[i] = data[i + 1];
+                    data[i + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+
+            swapped = false;
+
+            for (int i = data.length - 1; i > 0; i--) {
+                if (data[i].compareTo(data[i - 1]) < 0) {
+                    T temp = data[i];
+                    data[i] = data[i - 1];
+                    data[i - 1] = temp;
+
+                    swapped = true;
+                }
+            }
+        }while (swapped) ;
     }
+
+
 
 
     public static <T extends Comparable<T>> void shellSort(T[] data) {
 
-        // TODO implement Shell Sort here
+        System.out.println("Didn't do");
+
+        /*
+        int[] gaps = {701, 301, 132, 57, 23, 10, 4, 1};
+        int n = data.length;
+        foreach(){
+            for (int i = gap; i < n; i+=1){
+                for ( int j = i; )
+            }
+        }
+        */
+
     }
 
     private static <T extends Comparable<T>> void swap(T[] data, int index1, int index2) {
